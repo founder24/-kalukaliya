9649aa9ea5fda5f5481e2d71a086e4c85a48502e
Dipakk32554 <65485778-dipakk32554@users.noreply.replit.com>
2026-09-20 08:58:20 +0000

    Enable local Playwright hydration checks: add Nix Chromium runtime libraries, ignore dist-ssr/Playwright report artifacts

--- replit.nix
+++ replit.nix
@@ -1,5 +1,30 @@
 {pkgs}: {
   deps = [
+    pkgs.libgbm
+    pkgs.gtk3
+    pkgs.libxkbcommon
+    pkgs.xorg.libxshmfence
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXext
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libxcb
+    pkgs.xorg.libX11
+    pkgs.pango
+    pkgs.cairo
+    pkgs.expat
+    pkgs.alsa-lib
+    pkgs.mesa
+    pkgs.libdrm
+    pkgs.cups
+    pkgs.at-spi2-core
+    pkgs.at-spi2-atk
+    pkgs.atk
+    pkgs.dbus
+    pkgs.nspr
+    pkgs.nss
+    pkgs.glib
     pkgs.wrangler_1
     pkgs.google-cloud-sdk
     pkgs.chromium
